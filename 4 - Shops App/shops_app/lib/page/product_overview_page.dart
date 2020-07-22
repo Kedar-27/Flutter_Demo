21a511542d2634a65437a7b2ee66be051fffab8a
Kedar27 <kedarjsukerkar@yahoo.co.in>
2020-07-23 03:02:09 +0530

    Fonts added.

--- 4 - Shops App/shops_app/lib/page/product_overview_page.dart
+++ 4 - Shops App/shops_app/lib/page/product_overview_page.dart
@@ -4,7 +4,22 @@ import '../widget/product_item.dart';
 
 class ProductOverviewPage extends StatelessWidget {
   //region Properties
-  List<Product> loadedProducts = [];
+  List<Product> loadedProducts = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trous;
 
   //endregion
 
